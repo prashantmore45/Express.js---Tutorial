d9c193e4ecd732df13c23952afba8972506b4de5
prashantmore45 <prashantmorepm05@gmail.com>
2025-10-03 12:03:04 +0530

    Update .gitignore to include .env and modify app.js to use environment variable for PORT

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from 'express';
 
 const app = express();
 
-const PORT = 3000;
-
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'));
 
 app.get('/about', (req, res) => res.send('<h1>About Page</h1>'));
 
 app.get('/contact', (req, res) => res.send('<h1>Contact Page</h1>'));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
