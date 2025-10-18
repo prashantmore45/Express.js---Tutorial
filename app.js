d789a87e5f7d06b659782260d3b24022f8b85fd4
prashantmore45 <prashantmorepm05@gmail.com>
2025-10-18 19:31:45 +0530

    Refactor environment variable handling and add port validation using Zod

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {PORT} from './env.js'
 
 const app = express();
 
@@ -8,6 +9,6 @@ app.get('/about', (req, res) => res.send('<h1>About Page</h1>'));
 
 app.get('/contact', (req, res) => res.send('<h1>Contact Page</h1>'));
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
---
+++ env.js
@@ -0,0 +1,31 @@
+// export const PORT = isNaN(process.env.PORT) ? 3000 : parseInt(process.env.PORT);
+
+import { z, ZodError } from 'zod';
+
+const ageSchema = z.number().min(18).max(100).int();
+const userAge = 19;
+
+const parseUserAge = ageSchema.parse(userAge);
+console.log(parseUserAge);
+
+// const { data, error, success } = ageSchema.safeParse(userAge);
+
+// console.log(error);
+
+// try {
+//     const parsedUserAge = ageSchema.parse(userAge);
+//     console.log(parseUserAge);
+// } catch (error) {
+
+//     if (error instanceof ZodError) {
+//         console.log(error.errors[0].message);
+//     } else {
+//         console.log("Unexpected error:", error);
+//     }
+// }
+
+const portSchema = z.coerce.number().min(1).max(65535).default(3000);
+
+const PORT = portSchema.parse(process.env.PORT);
+
+console.log(`Server is running at port: ${PORT}`);
